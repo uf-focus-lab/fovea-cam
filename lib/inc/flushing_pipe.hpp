4c5d96bfef3efb79b07c222086b5599ccefd1ea0
Yuxuan Zhang <zyx@z-yx.cc>
2023-10-12 06:59:56 +0000

    update

--- lib/inc/flushing_pipe.hpp
+++ lib/inc/flushing_pipe.hpp
@@ -9,7 +9,7 @@ namespace FlushingPipe {
 
 class PipeEnd : public std::exception {};
 
-template <class T> class Pipe {
+template <typename T> class Pipe {
 private:
   std::mutex mutex;
   std::condition_variable cond_next;
@@ -23,5 +23,45 @@ public:
   void write(const T &value);
   T read();
 };
+
+template <typename T> void Pipe<T>::clear_buffer() {
+  if (buffer != nullptr) {
+    delete buffer;
+    buffer = nullptr;
+  }
+}
+
+template <typename T> void Pipe<T>::write(const T &value) {
+  std::unique_lock<std::mutex> lock(mutex);
+  if (!open)
+    throw PipeEnd();
+  clear_buffer();
+  buffer = new T(value);
+  cond_next.notify_one();
+  lock.unlock();
+}
+
+template <typename T> void Pipe<T>::close() {
+  std::unique_lock<std::mutex> lock(mutex);
+  clear_buffer();
+  open = false;
+  cond_next.notify_one();
+  lock.unlock();
+}
+
+template <typename T> T Pipe<T>::read() {
+  std::unique_lock<std::mutex> lock(mutex);
+  do {
+    if (!open)
+      throw PipeEnd();
+    if (buffer != nullptr)
+      break;
+    cond_next.wait(lock);
+  } while (1);
+  T value = *buffer;
+  clear_buffer();
+  lock.unlock();
+  return value;
+}
 
 } // namespace FlushingPipe
