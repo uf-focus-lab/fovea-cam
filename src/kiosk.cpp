565432922fb89be2a08ad383bd7c30ebb4b035d2
Yuxuan Zhang <github@z-yx.cc>
2024-01-08 16:20:34 +0000

    improva kiosk text rendering

--- src/kiosk.cpp
+++ src/kiosk.cpp
@@ -1,10 +1,11 @@
 #include <cmath>
 #include <glob.h>
 #include <iostream>
+#include <opencv2/core.hpp>
 #include <opencv2/core/types.hpp>
+#include <opencv2/imgproc.hpp>
 #include <string>
 #include <sys/wait.h>
-#include <thread>
 #include <unistd.h>
 
 #include "graphics/canvas.h"
@@ -23,6 +24,18 @@ double clamp(double min, double max, double val) {
     return max;
   else
     return val;
+}
+
+std::string str(std::string prefix, double val, int precision = 2) {
+  std::stringstream stream;
+  stream << prefix << std::fixed << std::setprecision(precision) << val;
+  return stream.str();
+}
+
+std::string str(double val, int precision = 2) {
+  std::stringstream stream;
+  stream << std::fixed << std::setprecision(precision) << val;
+  return stream.str();
 }
 
 class Tile {
@@ -58,14 +71,32 @@ public:
   Tile &fill(cv::Scalar color, double x2 = 1, double y2 = 1) {
     return fill(color, 0, x2, 0, y2);
   }
-  Tile &text(std::string text, cv::Scalar color, double scale = 3) {
+  Tile &text(std::string text, cv::Scalar color, double pad = 0.25) {
+    const unsigned height = content.height;
     // Center position
-    const cv::Size size = cv::getTextSize(text, cv::FONT_HERSHEY_DUPLEX, scale,
-                                          rint(scale * 1.6), nullptr);
-    const unsigned x = (bbox.width - size.width) / 2,
-                   y = (bbox.height + size.height) / 2;
-    cv::putText(mat, text, cv::Point(x, y), cv::FONT_HERSHEY_DUPLEX, scale,
-                color, 2);
+    const int fs = content.height / 8, ft = fs * 1.2;
+    const cv::Size t_canvas_size =
+        cv::getTextSize(text, cv::FONT_HERSHEY_DUPLEX, fs, ft, nullptr);
+    // 2p = 1 - h / (h + 2x) => x = (1 - 1/(1 -2p)) - h) / 2
+    const int fp = rint(double(t_canvas_size.height) * pad / (1 - 2 * pad));
+    const cv::Size t_bleed_size =
+        cv::Size(t_canvas_size.width + fp * 2, t_canvas_size.height + fp * 2);
+    // Create canvas for text mask
+    cv::Mat mask(t_bleed_size, CV_8UC1, cv::Scalar(0));
+    cv::putText(mask, text, cv::Point(fp, t_bleed_size.height - fp),
+                cv::FONT_HERSHEY_DUPLEX, fs, cv::Scalar(255), ft);
+    // Resize to fit
+    const double scale = (double)height / (double)t_bleed_size.height;
+    std::cout << "scale=" << scale << std::endl;
+    cv::resize(mask, mask, cv::Size(), scale, scale, cv::INTER_AREA);
+    const cv::Size size = mask.size();
+    const cv::Rect roi =
+        cv::Rect((bbox.width - size.width) / 2, (bbox.height - size.height) / 2,
+                 size.width, size.height);
+    // Convert to colored mask
+    cv::Mat text_mat(size, CV_8UC4, color);
+    // Apply text mat with given color
+    cv::copyTo(text_mat, mat(roi), mask);
     return *this;
   }
   bool handle(PointerEvent pos) {
@@ -96,9 +127,9 @@ int run(graphics::X11FB &fb, const char *_argv[], double exp, double fps,
   std::vector<const char *> argv;
   if (exp < 0.01)
     exp = 0.01;
-  std::cout << "EXP=" << exp * 10 << " ";
+  std::cout << str("EXP=", exp * 10) << " ";
   if (fps > 0.1)
-    std::cout << "FPS=" << std::to_string((int)rint(fps * 110 - 10.5)) << " ";
+    std::cout << str("FPS=", fps * 110 - 10.5) << " ";
   std::cout << _argv[0] << " " << cmd;
   return 0;
 }
@@ -123,29 +154,33 @@ int kiosk(const char *argv[]) {
   canvas.clear().show(splash).apply(fb.buffer());
   fb.sync();
   // Prepare tiles for interaction
-  const unsigned w = fb.shape().w, h = fb.shape().h / 8;
+  const unsigned w = fb.shape().w, h = fb.shape().h / 8,
+                 pad = rint(double(h) / 8.f);
   const auto bg = cv::Scalar(32, 32, 32, 255),
              fg = cv::Scalar(128, 64, 32, 255),
              stroke = cv::Scalar(128, 192, 64, 255);
   // Tile for EXP slider
   unsigned y = fb.shape().h * 5 / 8;
-  auto exp = Tile(cv::Rect(0, y, w, h));
+  auto exp = Tile(cv::Rect(0, y, w, h), pad);
   exp.value = 1.0 / 10.0;
-  exp.fill(bg).fill(fg, exp.value).text("EXP = 1.0", stroke);
+  exp.fill(bg).fill(fg, exp.value).text(str("EXP = ", exp.value * 10), stroke);
   // Tile for FPS slider
   y += h;
-  auto fps = Tile(cv::Rect(0, y, w, h));
+  auto fps = Tile(cv::Rect(0, y, w, h), pad);
   fps.value = 0;
   fps.fill(bg).fill(fg, exp.value).text("FPS = ---", stroke);
   // Tile for action buttons
   y += h;
-  auto btn_move = Tile(cv::Rect(0, y, w / 4, h)).fill(bg).text("MOV", stroke),
+  auto btn_move =
+           Tile(cv::Rect(0, y, w / 4, h), pad).fill(bg).text("MOV", stroke),
        btn_track =
-           Tile(cv::Rect(w / 4, y, w / 4, h)).fill(bg).text("TRK", stroke),
-       btn_match =
-           Tile(cv::Rect(2 * w / 4, y, w / 4, h)).fill(bg).text("MCH", stroke),
-       btn_rec =
-           Tile(cv::Rect(3 * w / 4, y, w / 4, h)).fill(bg).text("CAP", stroke);
+           Tile(cv::Rect(w / 4, y, w / 4, h), pad).fill(bg).text("TRK", stroke),
+       btn_match = Tile(cv::Rect(2 * w / 4, y, w / 4, h), pad)
+                       .fill(bg)
+                       .text("MCH", stroke),
+       btn_rec = Tile(cv::Rect(3 * w / 4, y, w / 4, h), pad)
+                     .fill(bg)
+                     .text("CAP", stroke);
 
   std::cerr << LOG_NAME "Start interaction" << std::endl;
   canvas.show(exp.mat, exp.bbox)
@@ -161,52 +196,64 @@ int kiosk(const char *argv[]) {
     PointerEvent pos = fb.wait_pointer();
     // Check for corresponding tile
     if (exp.handle(pos)) {
-      std::string value = std::to_string(exp.value * 10);
-      exp.fill(bg).fill(fg, exp.value).text("EXP = " + value, stroke);
+      exp.fill(bg)
+          .fill(fg, exp.value)
+          .text(str("EXP = ", exp.value * 10), stroke);
       canvas.show(exp.mat, exp.bbox).apply(fb.buffer());
       fb.sync();
     }
     if (fps.handle(pos)) {
       std::string value =
-          fps.value > 0.1 ? std::to_string((int)rint(fps.value * 110 - 10.5))
-                          : "N/A";
+          fps.value > 0.1 ? str("FPS = ", fps.value * 110 - 10.5) : "N/A";
       fps.fill(bg).fill(fg, fps.value).text("FPS = " + value, stroke);
       canvas.show(fps.mat, fps.bbox).apply(fb.buffer());
       fb.sync();
     }
     if (btn_move.handle(pos)) {
       btn_move.fill(bg).fill(stroke).text("MOV", fg);
-      canvas.show(btn_move.mat, btn_move.bbox).apply(fb.buffer());
-      fb.sync();
-      std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      canvas.show(splash).apply(fb.buffer());
+      canvas.clear()
+          .show(splash)
+          .show(btn_move.mat, btn_move.bbox)
+          .show(btn_track.mat, btn_track.bbox)
+          .show(btn_match.mat, btn_match.bbox)
+          .show(btn_rec.mat, btn_rec.bbox)
+          .apply(fb.buffer());
       fb.sync();
       return run(fb, argv, exp.value, fps.value, "move");
     }
     if (btn_track.handle(pos)) {
       btn_track.fill(bg).fill(stroke).text("TRK", fg);
-      canvas.show(btn_track.mat, btn_track.bbox).apply(fb.buffer());
-      fb.sync();
-      std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      canvas.show(splash).apply(fb.buffer());
+      canvas.clear()
+          .show(splash)
+          .show(btn_move.mat, btn_move.bbox)
+          .show(btn_track.mat, btn_track.bbox)
+          .show(btn_match.mat, btn_match.bbox)
+          .show(btn_rec.mat, btn_rec.bbox)
+          .apply(fb.buffer());
       fb.sync();
       return run(fb, argv, exp.value, fps.value, "track");
     }
     if (btn_match.handle(pos)) {
       btn_match.fill(bg).fill(stroke).text("MCH", fg);
-      canvas.show(btn_match.mat, btn_match.bbox).apply(fb.buffer());
-      fb.sync();
-      std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      canvas.show(splash).apply(fb.buffer());
+      canvas.clear()
+          .show(splash)
+          .show(btn_move.mat, btn_move.bbox)
+          .show(btn_track.mat, btn_track.bbox)
+          .show(btn_match.mat, btn_match.bbox)
+          .show(btn_rec.mat, btn_rec.bbox)
+          .apply(fb.buffer());
       fb.sync();
       return run(fb, argv, exp.value, fps.value, "match");
     }
     if (btn_rec.handle(pos)) {
       btn_rec.fill(bg).fill(stroke).text("CAP", fg);
-      canvas.show(btn_rec.mat, btn_rec.bbox).apply(fb.buffer());
-      fb.sync();
-      std::this_thread::sleep_for(std::chrono::milliseconds(200));
-      canvas.show(splash).apply(fb.buffer());
+      canvas.clear()
+          .show(splash)
+          .show(btn_move.mat, btn_move.bbox)
+          .show(btn_track.mat, btn_track.bbox)
+          .show(btn_match.mat, btn_match.bbox)
+          .show(btn_rec.mat, btn_rec.bbox)
+          .apply(fb.buffer());
       fb.sync();
       return run(fb, argv, exp.value, fps.value, "capture");
     }
