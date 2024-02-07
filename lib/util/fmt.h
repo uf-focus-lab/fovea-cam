b2df83ebeaad5dabd715210b1e6ded1972ab114a
Yuxuan Zhang <github@z-yx.cc>
2024-02-06 23:56:43 -0500

    util/fmt: supports sign switch

--- lib/util/fmt.h
+++ lib/util/fmt.h
@@ -4,11 +4,15 @@
 #include <sstream>
 #include <string>
 
-static inline std::string fmt(double val, unsigned n, unsigned p, char fill = ' ') {
-  char sign = val >= 0 ? '+' : '-';
-  val = std::abs(val);
+static inline std::string fmt(double val, unsigned n, unsigned p,
+                              char fill = ' ', bool sign = true) {
+  std::string s = "";
+  if (sign) {
+    s = val >= 0 ? '+' : '-';
+    val = std::abs(val);
+  }
   std::stringstream ss;
-  ss << sign << std::fixed << std::setprecision(p) << std::setw(n + p + 1)
+  ss << s << std::fixed << std::setprecision(p) << std::setw(n + p + 1)
      << std::setfill(fill) << val;
   return ss.str();
 }
